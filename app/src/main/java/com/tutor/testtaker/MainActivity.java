b722d8b328ecb7059e9c6856f27b5684be7c133a
rashid <rashid28hasan@gmail.com>
2019-10-23 17:02:39 +0600

    added login lock timer

--- app/src/main/java/com/tutor/testtaker/MainActivity.java
+++ app/src/main/java/com/tutor/testtaker/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView Info;
     private Button Login;
     private int counter = 5;
+    private CountDownTimer lockTimer;
+    private TextView txtLockTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +44,23 @@ public class MainActivity extends AppCompatActivity {
                 validate(Name.getText().toString(), Password.getText().toString());
             }
         });
+        txtLockTimer=findViewById(R.id.txtLockTimer);
+        lockTimer = new CountDownTimer(16000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d(TAG, "onTick: running");
+                 txtLockTimer.setText(String.valueOf(millisUntilFinished/1000));
+                Log.d(TAG, "onTick: running ended");
+            }
+            
+            @Override
+            public void onFinish() {
+                txtLockTimer.setVisibility(View.GONE);
+                Login.setEnabled(true);
+                counter=5;
+                Info.setText("No of attempts remaining: " + counter);
+            }
+        };
     }
 
     private void validate(String userName, String userPassword){
@@ -56,6 +76,9 @@ public class MainActivity extends AppCompatActivity {
 
             if(counter == 0){
                 Login.setEnabled(false);
+                txtLockTimer.setVisibility(View.VISIBLE);
+                lockTimer.start();
+                Log.d(TAG, "validate: timer started");
             }
         }
     } }
