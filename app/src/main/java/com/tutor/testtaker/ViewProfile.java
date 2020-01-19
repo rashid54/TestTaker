a9b33571624eaa0bb4907033e717902a55c5959e
rsd <rashid28hasan@gmail.com>
2020-01-20 03:21:33 +0600

    modi

--- app/src/main/java/com/tutor/testtaker/ViewProfile.java
+++ app/src/main/java/com/tutor/testtaker/ViewProfile.java
@@ -4,26 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ViewProfile extends AppCompatActivity {
-
+    public static final String TAG="viewprofile";
       private Button home;
-       private UserData userData;
-       private TextView name;
-       private TextView email;
-       private  TextView id;
+      private TextView name;
+      private TextView email;
+      private  TextView id;
+
+      private UserData userData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
-        userData=new UserData(this);
         home=findViewById(R.id.btnhome);
         name=findViewById(R.id.textView1);
         email=findViewById(R.id.textView2);
         id=findViewById(R.id.textView3);
+        userData= new UserData(this);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,10 +52,33 @@ public class ViewProfile extends AppCompatActivity {
 
             }
         });
-        UserProfile userprofile ;
-        userprofile=userData.getUser();
-        name.setText(userprofile.getUsername());
-        email.setText(userprofile.getEmail());
-        id.setText(userprofile.getId());
+
+        String url= "https://presslu1.pythonanywhere.com/api/getuser/";
+        JsonObjectRequest jsonObjectRequest= new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "onResponse: started");
+                try {
+                    name.setText(response.getString("username"));
+                    email.setText(response.getString("email"));
+                    id.setText(response.getString("id"));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse:couldn't save user info to sharedpreferences ");
+                Toast.makeText(ViewProfile.this, "Couldn't load profile info", Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> data= new HashMap<>();
+                data.put("Authorization","token "+userData.getAuthToken());
+                return data;
+            }
+        };
     }
 }
