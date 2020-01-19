94d92da3ac61070c03a34ebe34968cf866554a4e
rsd <rashid28hasan@gmail.com>
2020-01-20 05:09:56 +0600

    added postTestApi

--- app/src/main/java/com/tutor/testtaker/CreateTest.java
+++ app/src/main/java/com/tutor/testtaker/CreateTest.java
@@ -9,8 +9,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.JsonArray;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateTest extends AppCompatActivity implements CreateQuestionDialog.CreateQuesion {
     private static final String TAG = "CreateTest";
@@ -23,6 +39,7 @@ public class CreateTest extends AppCompatActivity implements CreateQuestionDialo
     private ArrayList<Ques> queslist;
     private ArrayList<Integer> quesIDlist;
     QuesAdapter quesAdapter;
+    UserData userData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +72,8 @@ public class CreateTest extends AppCompatActivity implements CreateQuestionDialo
 
         queslist= new ArrayList<>();
         quesIDlist= new ArrayList<>();
+
+        userData= new UserData(this);
     }
 
     @Override
@@ -62,5 +81,41 @@ public class CreateTest extends AppCompatActivity implements CreateQuestionDialo
         quesIDlist.add(id);
         queslist.add(ques);
         quesAdapter.setQueslist(queslist);
+    }
+
+    public void postTestApi(String testname,int duration,ArrayList<Integer> quesIDlist){
+        String url= "https://presslu1.pythonanywhere.com/api/test/";
+        JSONArray quesidarray=new JSONArray(quesIDlist);
+        JSONObject jsonObject=new JSONObject();
+        try {
+            jsonObject.put("testname",testname);
+            jsonObject.put("duration",duration);
+            jsonObject.put("questions",quesidarray);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest= new JsonObjectRequest(Request.Method.POST, url, jsonObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "onResponse: started");
+                Toast.makeText(CreateTest.this, "Test Created Successfully", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: started");
+            }
+        }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> map= new HashMap<>();
+                map.put("Authorization","token "+userData.getAuthToken());
+                return map;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(jsonObjectRequest);
+        requestQueue.start();
     }
 }
